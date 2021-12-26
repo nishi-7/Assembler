02d4c4dad5563935df16acf8cc400fd31e23ac88
kadu-v <kazuonsunday@yahoo.co.jp>
2021-12-26 23:11:37 +0900

    Add: make symboltable

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,11 @@
-
 import assembler._
 import error.SyntaxError
 
 object Main {
-    def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-        val code =
-            """A;JGT
+    val code =
+      """A;JGT
               |(xxx)
               |@1234
               |@label
@@ -14,19 +13,17 @@ object Main {
               |0;JMP
               |D;JEQ
               |""".stripMargin
-        val lex = new Lexer(code)
-        val psr = new Parser(lex)
+    val lex = new Lexer(code)
+    val psr = new Parser(lex)
+    val mst = new SymbolTable(psr)
+    val (asms, table) = mst.makeSymbolTable()
 
-        while(!psr.isEnd()) {
-            val cmd = try {
-                psr.parseOne()
-            } catch {
-                case e: SyntaxError => {
-                    println(e.errorReport(code))
-                    return
-                }
-            }
-            println(cmd)
-        }
+    for (asm <- asms) {
+      println(asm)
     }
+
+    for ((label, counter) <- table) {
+      println(s"${label} -> ${counter}")
+    }
+  }
 }
---
+++ src/main/scala/assembler/SymbolTable.scala
@@ -0,0 +1,22 @@
+package assembler
+
+class SymbolTable(psr: Parser) {
+  def makeSymbolTable(): (Vector[Asm], Map[String, Int]) = {
+    var counter = 0
+    var table: Map[String, Int] = Map.empty
+    var asms: Vector[Asm] = Vector.empty
+    while (!psr.isEnd()) {
+      val cmd = psr.parseOne()
+      cmd.ty match {
+        case ACmdLabel(Symbol(label)) => {
+          table = table.updated(label, counter)
+        }
+        case _ => {
+          counter += 1
+          asms = asms :+ cmd
+        }
+      }
+    }
+    (asms, table)
+  }
+}
