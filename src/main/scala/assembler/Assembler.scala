caba3fe250187ac0bdc69e1ca134d68b6800312a
kadu-v <kazuonsunday@yahoo.co.jp>
2022-03-31 21:26:52 +0900

    Remove: redundant code

--- src/main/scala/assembler/Assembler.scala
+++ src/main/scala/assembler/Assembler.scala
@@ -7,7 +7,6 @@ class Assembler(code: String) {
     val psr = new Parser(lex)
     val mst = new SymbolTable(psr)
     val (asms, table) = mst.makeSymbolTable()
-    asms.map(println)
     val cwr = new CodeWriter(asms, table)
     cwr.codeGen()
   }
